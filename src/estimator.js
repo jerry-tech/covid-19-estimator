83e457fc6f9c33f73877efb820b8f0661d68d7a4
jerry-tech <53123555+jerry-tech@users.noreply.github.com>
2020-04-11 16:17:28 -0700

    commit

--- src/estimator.js
+++ src/estimator.js
@@ -5,44 +5,69 @@ const covid19ImpactEstimator = (data) => {
     impact: {}, // your best case estimation
     severeImpact: {} // your severe case estimation
   };
-    // estimate the number of currently infected for IMPACT && severeImpact
+  // estimate the number of currently infected for IMPACT && severeImpact
   record.impact.currentlyInfected = data.reportedCases * 10;
   record.severeImpact.currentlyInfected = data.reportedCases * 50;
 
+
   // infectionsByRequestedTime for impact && severeImpact
-  record.impact.infectionsByRequestedTime = record.impact.currentlyInfected * 512;
-  record.severeImpact.infectionsByRequestedTime = record.severeImpact.currentlyInfected * 512;
+  let calDay = null;
+  const period = data.timeToElapse;
+  if(data.periodType === 'days'){
+    calDay = period;
+  }else if(data.periodType === 'weeks'){
+    calDay = period * 7;
+  }else if(data.periodType === 'month'){
+    calDay = period * 30;
+  }
+  const factor = (calDay / 3);
+  record.impact.infectionsByRequestedTime = Math.floor(record.impact.currentlyInfected * (2 ** factor));
+  record.severeImpact.infectionsByRequestedTime = Math.floor(record.severeImpact.currentlyInfected * (2 ** factor));
 
   // 15% percent calculation of impact and severe impact
-  record.impact.severeCasesByRequestedTime = 0.15
-    * record.impact.infectionsByRequestedTime;
-  record.severeImpact.severeCasesByRequestedTime = 0.15
-    * record.severeImpact.infectionsByRequestedTime;
+  record.impact.severeCasesByRequestedTime = Math.floor(0.15
+    * record.impact.infectionsByRequestedTime);
+  record.severeImpact.severeCasesByRequestedTime = Math.floor(0.15
+    * record.severeImpact.infectionsByRequestedTime);
 
   // total number of hospitalBedsByRequestedTime for impact && severe impact
-  record.impact.hospitalBedsByRequestedTime = Math.round((0.35 * data.totalHospitalBeds)
-     - record.impact.severeCasesByRequestedTime);
-  record.severeImpact.hospitalBedsByRequestedTime = Math.round((0.35 * data.totalHospitalBeds)
-     - record.severeImpact.severeCasesByRequestedTime);
+  record.impact.hospitalBedsByRequestedTime = Math.floor((0.35 * data.totalHospitalBeds)
+    - record.impact.severeCasesByRequestedTime);
+  record.severeImpact.hospitalBedsByRequestedTime = Math.floor((0.35 * data.totalHospitalBeds)
+    - record.severeImpact.severeCasesByRequestedTime);
 
   // casesForICUByRequestedTime for impact && severe impact
-  record.impact.casesForICUByRequestedTime = 0.05 * record.impact.infectionsByRequestedTime;
-  record.severeImpact.casesForICUByRequestedTime = 0.05
-    * record.severeImpact.infectionsByRequestedTime;
+  record.impact.casesForICUByRequestedTime = Math.floor(0.05 * record.impact.infectionsByRequestedTime);
+  record.severeImpact.casesForICUByRequestedTime = Math.floor(0.05
+    * record.severeImpact.infectionsByRequestedTime);
 
   // casesForVentilatorsByRequestedTime for impact && severe impact
-  record.impact.casesForVentilatorsByRequestedTime = 0.02
-    * record.impact.infectionsByRequestedTime;
-  record.severeImpact.casesForVentilatorsByRequestedTime = 0.02
-    * record.severeImpact.infectionsByRequestedTime;
+  record.impact.casesForVentilatorsByRequestedTime = Math.floor(0.02
+    * record.impact.infectionsByRequestedTime);
+  record.severeImpact.casesForVentilatorsByRequestedTime = Math.floor(0.02
+    * record.severeImpact.infectionsByRequestedTime);
 
   // estimation of how much money the economy is likely to lose daily form impact && severe impact
-  record.impact.dollarsInFlight = Math.round((record.impact.infectionsByRequestedTime
-         * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD) / 100);
-  record.severeImpact.dollarsInFlight = Math.round((record.severeImpact.infectionsByRequestedTime
-        * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD) / 100);
+  record.impact.dollarsInFlight = Math.floor((record.impact.infectionsByRequestedTime
+    * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD));
+  record.severeImpact.dollarsInFlight = Math.floor((record.severeImpact.infectionsByRequestedTime
+    * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD));
 
   return record;
 };
 
+// const data = {
+//   region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 4,
+//     avgDailyIncomePopulation: 0.73
+//   },
+//   periodType: 'days',
+//   timeToElapse: 38,
+//   reportedCases: 2747,
+//   population: 92931687,
+//   totalHospitalBeds: 678874
+// };
+// covid19ImpactEstimator(data);
 export default covid19ImpactEstimator;
