3be8b9016002b9f0c1dcfc1c525779dca5cba9a8
jerry-tech <53123555+jerry-tech@users.noreply.github.com>
2020-04-12 00:19:14 -0700

    commit

--- src/estimator.js
+++ src/estimator.js
@@ -59,18 +59,18 @@ const covid19ImpactEstimator = (data) => {
   return record;
 };
 
-// const data = {
-//   region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 4,
-//     avgDailyIncomePopulation: 0.73
-//   },
-//   periodType: 'days',
-//   timeToElapse: 38,
-//   reportedCases: 2747,
-//   population: 92931687,
-//   totalHospitalBeds: 678874
-// };
-// covid19ImpactEstimator(data);
+const data = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 4,
+    avgDailyIncomePopulation: 0.73
+  },
+  periodType: 'days',
+  timeToElapse: 38,
+  reportedCases: 2747,
+  population: 92931687,
+  totalHospitalBeds: 678874
+};
+covid19ImpactEstimator(data);
 export default covid19ImpactEstimator;
